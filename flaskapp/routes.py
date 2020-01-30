8ac3a4644a26b004954f7168feb89a12c43220ff
Avani <avani@crazyideas.co.in>
2020-01-30 17:37:46 +0100

    login form changes

--- flaskapp/routes.py
+++ flaskapp/routes.py
@@ -1,6 +1,6 @@
 from flask import render_template
-
 from flaskapp import app
+from flaskapp.forms import LoginForm
 
 
 @app.route('/')
@@ -17,4 +17,10 @@ def index():
             'body': 'The Avengers movie was cool!'
         }
     ]
-    return render_template('index.html', title='Home', user=user, posts=posts)
+    return render_template('index.html', user=user, posts=posts)
+
+
+@app.route('/login')
+def login():
+    form = LoginForm()
+    return render_template('login.html', title='Sign In', form=form)
